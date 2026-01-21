9da021fb7ded0a95c0d55c829c385115dd46a93f
Armanalam7790 <alamarman7790@gmail.com>
2026-01-21 22:48:09 +0530

    create dock

---
+++ src/components/Dock.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import "./dock.scss"
+
+const Dock = () => {
+  return (
+    <footer className='dock'>
+        <div className="icon github"> <img src="/github.svg" alt="" /></div>
+        <div className="icon note"> <img src="/note.svg" alt="" /></div>
+        <div className="icon pdf"> <img src="/pdf.svg" alt="" /></div>
+        <div className="icon calender"> <img src="/calender.svg" alt="" /></div>
+        <div className="icon spotify" > <img src="/spotify.svg" alt="" /></div>
+        <div className="icon mail"> <img src="/mail.svg" alt="" /></div>
+        <div className="icon link" > <img src="/link.svg" alt="" /></div>
+        <div className="icon cli" > <img src="/cli.svg" alt="" /></div>
+
+
+
+
+    </footer>
+  )
+}
+
+export default Dock
